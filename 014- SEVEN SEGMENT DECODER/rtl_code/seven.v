31fe879a251285c3c15e69b686cfe87ba414d8cc
Nidhin Chandran <96820094+Nidhinchandran47@users.noreply.github.com>
2024-06-03 22:51:53 +0530

    Update seven.v

--- 014- SEVEN SEGMENT DECODER/rtl_code/seven.v
+++ 014- SEVEN SEGMENT DECODER/rtl_code/seven.v
@@ -27,12 +27,14 @@ module seven(
     input d,
     output [6:0] out
     );
-    assign out [0] = (~b&~d) | (~a&c) | (b&c) | (a&~d) | (~a&b&d) | (a&~b&~c) | (a&~b&~c&~d);
-    assign out [1] = ~(a|b) | ~(a|c|d) | (~a&c&d) | (a&~c&d) | (a&c&~d) | (a&~b&~c&~d);
-    assign out [2] = ~(a|c) | (~a&d) | (~c&d) | (~a&b) | (a&~b&c) | (a&~b&~c&~d);
-    assign out [3] = ~(a|b|d) | (~a&~b&c) | (b&~c&d) | (b&~c&d) | (a&~b&d) | (a&b&~c) | (a&~b&~c&~d);
-    assign out [4] = (c&~d) | (a&c) | (a&b) | (~a&~b&~d) | (a&~b&~c&~d);
-    assign out [5] = (b&~d) | (a&c) |(~a&~c&~d) |(~a&b&~c) |(a&~b&d) | (a&~b&~c&~d);
-    assign out [6] = (~b&c) | (~d&c) |(a&~b) |(a&d) |(~a&b&~c);
+    
+    assign out[0] = (~b & ~d) | (~a & c) | (b & c) | (a & ~d) | (~a & b & d) | (a & ~b & ~c);
+    assign out[1] = (~a & ~b) | (~a & ~c & ~d) | (~a & c & d) | (a & ~c & d) | (a & c & ~d);
+    assign out[2] = (~a & ~c) | (~a & d) | (~c & d) | (~a & b) | (a & ~b & c);
+    assign out[3] = (~a & ~b & c) | (b & ~c & d) | (a & ~b & d) | (a & b & ~c) | (a & ~b & ~c);
+    assign out[4] = (c & ~d) | (a & c) | (a & b) | (~a & ~b & ~d);
+    assign out[5] = (b & ~d) | (a & c) | (~a & ~c & ~d) | (~a & b & ~c) | (a & ~b & d);
+    assign out[6] = (~b & c) | (~d & c) | (a & ~b) | (a & d) | (~a & b & ~c);
     
 endmodule
+
